e7acaff7e6d3d307943bd9956623c618794969a5
EdKung <edkung0728@gmail.com>
2017-02-16 00:07:27 +0800

    add the project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # newsCrawler
+
+A simple spider(web crawler) program in Node.js.
+
+This spider will find those articles related to user-input keyword from CNN.
+
+## How to crawl
+```javascript
+node newsCrawler.js "keyword" "sitesNum"
+```
+
+* **keyword** is the keyword to search
+* **sitesNum** is the maximal number of successfully reached sites
---
+++ newsCrawler.js
@@ -0,0 +1,109 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var url_parse = require('url-parse');
+
+var START_URL = "http://edition.cnn.com";
+var SEARCH_WORD = "love";
+var MAX_SITES_TO_VISIT = 30;
+
+var sitesToVisit = [];
+var sitesVisited = {};
+var numSucceedToVisit = 0;
+var result = [];
+
+var url = new url_parse(START_URL);
+var baseUrl = url.protocol + "//" + url.hostname;
+console.log("BaseUrl: " + baseUrl);
+
+var main = (function(){
+    process.argv.forEach(function(val,index,array) {
+      switch(index) {
+        case 2:
+          SEARCH_WORD = val;
+          break;
+        case 3:
+          MAX_SITES_TO_VISIT = parseInt(20);
+          break;
+        default:
+          break;
+      }
+    });
+
+    console.log("===================================");
+    console.log("|                                 |");
+    console.log("|     The crawler is starting     |");
+    console.log("|                                 |");
+    console.log("===================================");
+    console.log("|                                 |");
+    console.log("|     Searching Word..." + SEARCH_WORD);
+    console.log("|                                 |");
+    console.log("===================================");
+
+    sitesToVisit.push(START_URL);
+
+    crawl();
+})();
+
+function crawl() {
+  if(numSucceedToVisit >= MAX_SITES_TO_VISIT) {
+    console.log("===================================");
+    console.log("|                                 |");
+    console.log("|             Result              |");
+    console.log("|                                 |");
+    console.log("===================================");
+    console.log("After crawling max limit of number of pages,");
+    console.log("We found " + result.length + " related articles!");
+    console.log("The followings are the results");
+    console.log(result.join('\r\n'));
+    console.log("===================================");
+    console.log("|                                 |");
+    console.log("|            Finished             |");
+    console.log("|                                 |");
+    console.log("===================================");
+    return;
+  }
+  var nextSite = sitesToVisit.pop();
+  nextSite = nextSite.endsWith('/') ? nextSite.slice(0,-1) : nextSite;
+  if(nextSite in sitesVisited) {
+    crawl();
+  } else {
+    visitSite(nextSite);
+  }
+};
+
+function visitSite(url) {
+  // mark visted sites in the sitesVisited
+  sitesVisited[url] = true;
+  numSucceedToVisit++;
+
+  // console.log("Visiting site ---> " + url);
+
+  request({
+    uri: url
+  }, (err, response, body) => {
+
+    if(response.statusCode !== 200) {
+      // console.log("Fail to request, status: " + response.statusCode);
+      numSucceedToVisit--;
+    } else {
+      var $ = cheerio.load(body);
+      var isFound = searchKeyword($, SEARCH_WORD);
+      if(isFound){
+        console.log("Found!!! at the site --> " + url + "\n");
+        result.push(url);
+      }
+      // append new sites to the sitesToVisit
+      var sites = $("a[href^='/']");
+      // console.log("Found " + sites.length + " new sites");
+      sites.each(function(){
+        sitesToVisit.push(baseUrl + $(this).attr("href"));
+      });
+    }
+    crawl();
+  });
+}
+
+function searchKeyword($, keyword) {
+  var body = $("html").find("body").text().toLowerCase();
+  return (body.indexOf(keyword.toLowerCase()) !== -1);
+}
